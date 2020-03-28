45f5b9284c14bba340ddbb329451580ff37fee73
Rhymond <raimondas.kazlauskas@couchbase.com>
2020-03-28 11:16:52 +0000

    remove redundant files

--- src/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button } from 'react-bulma-components';
-
-export default ({ children }) => (
-  <Button color="primary">{children}</Button>
-)
